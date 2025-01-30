19689da15761b5dff53936e3ce097519e1c79561
SaekoF <2201110053ub@cyber-u.ac.jp>
2025-01-30 10:33:31 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,23 +1,27 @@
 var express = require('express');
- var router = express.Router();
+var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 
-
- // 接続情報を設定
+// 接続情報を設定
 const { MongoClient } = require("mongodb");
- const uri = "******";
- const client = new MongoClient(uri);
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
- router.get('/', async (req, res) => {
- // データベース、コレクションを指定
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
 const database = client.db('notes');
- const notes = database.collection('notes');
+const notes = database.collection('notes');
 
- // idが１のドキュメントを取得
+// idが１のドキュメントを取得
 const query = { id: 2 };
- const note = await notes.findOne(query);
+const note = await notes.findOne(query);
 
- res.json(note);
- })
+res.json(note);
+})
 
- module.exports = router;
+module.exports = router;
