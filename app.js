51459d566261ee866a00d26ac4ec5e4d880e51c4
SaekoF <2201110053ub@cyber-u.ac.jp>
2025-01-30 11:21:05 +0900

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
-// view engine setup
+// view engine setup設定
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
